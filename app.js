975e6d2844a7322275fb0476e2dd7803ba6ec56e
David <david.diez31@free.fr>
2017-01-06 12:40:51 +0100

    exercice ok

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // good luck !
+
+$(document).ready(function() {
+
+    var btn = $("input");
+    for (var i = 0; i < btn.length; i++) {
+        var btnColor = $(btn[i]).attr("data-color");
+        $(btn[i]).css("background", btnColor);
+    }
+
+    $(btn).click(function() {
+        var bgColor = $(this).attr("data-color");
+        if ($(":checkbox").is(":checked")) {
+            $(".main").css("color", bgColor);
+        } else {
+            $("body").css("background", bgColor);
+        }
+    });
+});
